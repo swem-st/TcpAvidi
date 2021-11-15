5a2381966e25d46591e1787b34ffe9d43fefccb2
swem-st <61904722+swem-st@users.noreply.github.com>
2021-11-15 15:43:25 +0200

    All

---
+++ Task TCP Avidi/ClientObject.cs
@@ -0,0 +1,63 @@
+﻿using Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ClientObject
+    {
+        public TcpClient client;
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+        public void Process()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                Random random = new Random();
+                Bitcoin bitcoinChange = new Bitcoin();
+
+                stream = client.GetStream();
+                byte[] data = new byte[64]; // буфер для получаемых данных
+                while (true)
+                {
+                    bitcoinChange.currentTime = DateTime.Now;
+                    bitcoinChange.price = random.Next();
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+
+                    Console.WriteLine(message);
+                    message = bitcoinChange.currentTime + " : " + bitcoinChange.price;
+                    data = Encoding.Unicode.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+    }
+}
